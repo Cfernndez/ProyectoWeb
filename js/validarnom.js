4c1cc944a138c6f0e1a698aa3f26a67cd0c2af81
Cfernndez <c.fernandezu@duocuc.cl>
2024-05-09 15:38:20 -0400

    cambios

--- js/validarnom.js
+++ js/validarnom.js
@@ -9,6 +9,10 @@ function validarForm() {
         alert('Por favor, ingrese un apellido válido.');
         return false;
     }
+    if (!validarRut(document.getElementById('rut').value)) {
+        alert('Por favor, ingrese un Rut válido.');
+        return false;
+    }
 
     if (!validarEmail(document.getElementById('mail').value)) {
         alert('Por favor, ingrese un correo electrónico válido.');
@@ -27,20 +31,18 @@ function validarForm() {
         return false;
     }
     if (!validarContraseña()) {
-
         alert('Las contraseñas no coinciden debe contener al menos 8 caracteres, una letra mayúscula y un número.');
         return false;
     }
+
     return true;
 }
 
 function validarNombreApellido(nombreApellido) {
-    // Expresión regular para validar nombre y apellido
-    const regex = /^[a-zA-ZÀ-ÿ\u00f1\u00d1\s']+$/;
+    // Expresión regular para validar nombre y apellido con un mínimo de 3 caracteres
+    const regex = /^[a-zA-ZÀ-ÿ\u00f1\u00d1\s']{3,}$/;
     return regex.test(nombreApellido);
 }
-
-
 
 function validarEmail(email) {
     // Expresión regular para validar correo electrónico
@@ -62,4 +64,30 @@ function validarContraseña() {
     // Expresión regular para validar que la contraseña tenga al menos 8 caracteres, una letra minúscula, una letra mayúscula y un número
     const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
     return pass1 === pass2 && regex.test(pass1);
+}
+
+function validarRut(rut) {
+    // Expresión regular para validar Rut chileno
+    const regex = /^(\d{1,2}(\.?\d{3}){1,2}-[\dkK])$/;
+    if (!regex.test(rut)) return false;
+
+    // Separar el Rut de su dígito verificador
+    const parts = rut.split('-');
+    let num = parts[0].split('.').join('');
+    const dv = parts[1].toUpperCase();
+
+    // Verificar que el dígito verificador coincida
+    let suma = 0;
+    let multiplo = 2;
+    for (let i = 1; i <= num.length; i++) {
+        const index = multiplo * parseInt(num.charAt(num.length - i));
+        suma += index;
+        if (multiplo < 7) multiplo += 1;
+        else multiplo = 2;
+    }
+
+    const dvEsperado = 11 - (suma % 11);
+    const dverificador = dvEsperado === 11 ? '0' : dvEsperado === 10 ? 'K' : dvEsperado.toString();
+
+    return dv === dverificador;
 }
